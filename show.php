fd66b3f71b6f65ed2649134c18565c779c650d88
AayushN01 <aayushniraula1@gmail.com>
2023-12-02 13:51:38 +0545

    Read Operation

--- show.php
+++ show.php
@@ -1,11 +1,27 @@
 <?php 
   include ('includes/template.php');
+  include('db.php');
+
+  if(isset($_GET['id']))
+  {
+    $patientId = $_GET['id'];
+    $query = "SELECT * FROM patients WHERE id=$patientId";
+    $result = mysqli_query($connection,$query);
+    if($result)
+    {
+        $patient = mysqli_fetch_assoc($result);
+        if(isset($patient['middle_name'])){
+            $short_middle = strtoupper(substr($patient['middle_name'], 0, 1)) . '.';
+        }
+       
+    }
+  }
 ?>
     <section class="p-5">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <h3>Patient Detail: <span class="text text-danger">(Patient Name)</span></h3>
+            <h3>Patient Detail: <span class="text text-danger"><?php echo $patient['first_name'] . ' ' .$short_middle . ' ' .$patient['last_name']; ?></span></h3>
             <a href="index.php" class="ms-auto btn btn-secondary" style="float: right;">Go Back</a>
           </div>
         </div>
@@ -13,29 +29,47 @@
           <div class="col-md-12">
             <div class="table-responsive my-2">
               <table class="table table-bordered table-hover">
-                <thead>
-                  <tr>
-                    <th width="5%">SNo.</th>
-                    <th width="30%">Name</th>
-                    <th width="30%">Email</th>
-                    <th width="5%">Gender</th>
-                    <th width="15%">Contact No.</th>
-                    <th width="15%">Actions</th>
-                  </tr>
-                </thead>
                 <tbody>
-                  <tr>
-                    <th>1</th>
-                    <td>Aayush Niraula</td>
-                    <td>aayushniraula1@gmail.com</td>
-                    <td>Male</td>
-                    <td>9841228968</td>
-                    <td>
-                      <a href="show.php" class="btn btn-primary btn-sm" title="View Details"><i class="fa fa-eye"></i></a>
-                      <a href="edit.php" class="btn btn-warning btn-sm" title="Edit Details"><i class="fa fa-pen"></i></a>
-                      <a href="#" class="btn btn-danger btn-sm" title="Delete"><i class="fa fa-trash"></i></a>
-                    </td>
-                  </tr>
+                    <tr>
+                        <th>Full Name</th>
+                        <td colspan="3"><?php echo $patient['first_name'] . ' ' .$patient['middle_name'] . ' ' .$patient['last_name']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Date Of Birth</th>
+                        <td><?php echo $patient['dob']; ?></td>
+                        <th>Gender</th>
+                        <td><?php echo $patient['gender']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Nationality</th>
+                        <td colspan="3"><?php echo $patient['nationality']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td><?php echo $patient['address']; ?></td>
+                        <th>Email</th>
+                        <td><?php echo $patient['email']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Contact Number</th>
+                        <td><?php echo $patient['contact_no'] ?></td>
+                        <th>Contact Number (alternate)</th>
+                        <td><?php echo $patient['contact_alternate']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Occupation</th>
+                        <td colspan="3"><?php echo $patient['occupation']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Medical History</th>
+                        <td colspan="3"><?php echo $patient['medical_history']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Created At</th>
+                        <td><?php echo $patient['created_at'] ?></td>
+                        <th>Updated At</th>
+                        <td><?php echo $patient['updated_at']; ?></td>
+                    </tr>
                 </tbody>
               </table>
             </div>
